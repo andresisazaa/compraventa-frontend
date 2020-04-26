62a4e004e5545fcc3b74d22e07e39371f41cce6c
Andrés Isaza <andres.isaza1506@gmail.com>
2020-04-26 13:30:57 -0500

    models service done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,16 @@
 
 export const environment = {
   production: false,
+  backendUrl: 'http://localhost:3000',
   firebase: {
-    apiKey: "AIzaSyB4KqHPy-QVqUkOXj-n53gIuZt0cZxEyac",
-    authDomain: "proyecto-ing-web.firebaseapp.com",
-    databaseURL: "https://proyecto-ing-web.firebaseio.com",
-    projectId: "proyecto-ing-web",
-    storageBucket: "proyecto-ing-web.appspot.com",
-    messagingSenderId: "542998837968",
-    appId: "1:542998837968:web:e1d57fc308210f3214bcd5"
-  }
+    apiKey: 'AIzaSyB4KqHPy-QVqUkOXj-n53gIuZt0cZxEyac',
+    authDomain: 'proyecto-ing-web.firebaseapp.com',
+    databaseURL: 'https://proyecto-ing-web.firebaseio.com',
+    projectId: 'proyecto-ing-web',
+    storageBucket: 'proyecto-ing-web.appspot.com',
+    messagingSenderId: '542998837968',
+    appId: '1:542998837968:web:e1d57fc308210f3214bcd5',
+  },
 };
 
 /*
